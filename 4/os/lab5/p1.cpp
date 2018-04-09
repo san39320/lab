3ccad436e13851058ffc26dc73d1bcb53d835096
sanjay <sanjayshetty2015@gmail.com>
2018-04-09 14:57:35 +0530

    kdjd

--- 4/os/lab5/p1.cpp
+++ 4/os/lab5/p1.cpp
@@ -5,7 +5,6 @@ using namespace std;
 #define left(i)({return(2*i+1);})
 #define right(i)({return(2*i+2);})
 #define parent(i)({return((i+1)/2-1);})
-#define fr(a,n) for(int i=a;i<n;i++)
 const int unoccupied=-1;
 const int split=-2;
 const int memory=1024;
@@ -16,14 +15,17 @@ int levels;
 int mem[100];
 void initialize(){
 	maxorder=(log(memory/blocksize)/log(2));
-	levels=maxorder+1;
+	levels=maxorder+1;//total number of levels
 	size=(pow(2,levels)-1);
 	memset(mem,-1,size*4);
 }
-void allocate(int process){}
+void allocate(int process){
+	int blocksizerequired=ceil()
+
+}
 int main(){
 	initialize();
 	int process[5]={63,64,128,32,200}
-	fr(0,5)allocate(process[i]);
+	fr(0,5)allocate(process[i],i);
 	return 1;
 }
